f40ec1b12483cd779313ec8d27920bc954e384f3
Parastoo Veisi <parastoo.veisi@gmail.com>
2021-11-04 13:56:35 -0600

    Add line chart

--- src/components/Graphs.svelte
+++ src/components/Graphs.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import BarChart from "./BarChart.svelte";
+  import LineChart from "./charts/LineChart.svelte";
 </script>
 
 <div class="grid grid-cols-2 mt-6">
   <div>Firts Graph here</div>
   <BarChart />
-  <div class="col-span-1">Third Graph here</div>
+  <div class="col-span-2"><LineChart /></div>
   <div />
 </div>
