61b9190326e50e8b722ed4a5fc11c2b9526881fd
Joe Krump <joe.krump@clio.com>
2021-11-04 11:26:29 -0400

    Start adding top nav

---
+++ src/components/TopNav.svelte
@@ -0,0 +1,18 @@
+<nav>
+  <ul class="flex justify-end items-center">
+    <li class="ml-1">
+      <button title="Search" class="rounded-full h-8 w-8 flex items-center justify-center bg-gray-200">🔎</button>
+    </li>
+    <li class="ml-1">
+      <button title="Chat" class="rounded-full h-8 w-8 flex items-center justify-center bg-gray-200">💬</button>
+    </li>
+    <li class="ml-1">
+      <button title="Help" class="rounded-full h-8 w-8 flex items-center justify-center bg-gray-200">?</button>
+    </li>
+    <li class="ml-1">
+      <span>Avatar</span>
+      <span>Name</span>
+      <span>Dropdown arrow</span>
+    </li>
+  </ul>
+</nav>
