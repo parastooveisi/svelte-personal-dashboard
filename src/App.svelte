1596569b56165907752f08e325f9a1b878d94945
Parastoo Veisi <parastoo.veisi@gmail.com>
2021-11-04 09:04:40 -0600

    Add nav bar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import TailwindCSS from "./TailwindCSS.svelte";
-  import HelloWorld from "./HelloWorld.svelte";
+  import NavBar from "./components/NavBar.svelte";
 </script>
 
 <TailwindCSS />
 
-<main>
-  <HelloWorld />
-</main>
-
-<style>
-</style>
+<div
+  class="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow"
+>
+  <NavBar />
+  <main role="main" class="w-full flex-grow pt-1 px-3">
+    <h1 class="text-3xl md:text-5xl mb-4 font-extrabold" id="home">
+      Personal Dashboard
+    </h1>
+  </main>
+</div>
