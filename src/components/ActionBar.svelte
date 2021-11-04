23ca7e42c9325826b65af1efe68d6df748eb7b30
Joe Krump <joe.krump@clio.com>
2021-11-04 12:52:43 -0400

    Action bar WIP

---
+++ src/components/ActionBar.svelte
@@ -0,0 +1,11 @@
+<div>
+  <div class="avatars">
+    <ul class="grid">
+      <li class="rounded-full h-10 w-10 flex items-center justify-center bg-indigo-700 text-gray-100">P</li>
+      <li class="rounded-full h-10 w-10 flex items-center justify-center bg-pink-700 text-gray-100">J</li>
+      <li class="rounded-full h-10 w-10 flex items-center justify-center bg-green-700 text-gray-100">M</li>
+      <li class="rounded-full h-10 w-10 flex items-center justify-center bg-blue-700 text-gray-100">W</li>
+      <li class="rounded-full h-10 w-10 flex items-center justify-center bg-black text-gray-100">C</li>
+    </ul>
+  </div>
+</div>
