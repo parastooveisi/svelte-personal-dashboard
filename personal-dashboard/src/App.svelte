90043953986307ac6b7ef744fcc5ab8b77655be2
Parastoo Veisi <parastoo.veisi@gmail.com>
2021-11-03 19:47:29 -0600

    Project boilerplate

---
+++ personal-dashboard/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import TailwindCSS from "./TailwindCSS.svelte";
+  import HelloWorld from "./HelloWorld.svelte";
+</script>
+
+<TailwindCSS />
+
+<main>
+  <HelloWorld />
+</main>
+
+<style>
+</style>
