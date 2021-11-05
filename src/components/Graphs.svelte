883dafa18f18886e07d7e55e10942833e80afcac
Joe Krump <joe.krump@clio.com>
2021-11-04 21:54:16 -0400

    Make charts responsive

--- src/components/Graphs.svelte
+++ src/components/Graphs.svelte
@@ -5,9 +5,11 @@
 </script>
 
 <div class="grid grid-cols-2 mt-6 gap-3">
-  <div><DonutChart /></div>
-  <div>
+  <div class="col-span-2 xl:col-span-1">
+    <DonutChart />
+  </div>
+  <div class="col-span-2 xl:col-span-1">
     <BarChart />
   </div>
-  <div class="col-span-2"><LineChart /></div>
+  <div class="col-span-2 shadow-lg p-4"><LineChart /></div>
 </div>
--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,23 +1,17 @@
-<script>
+<script lang="ts">
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let items;
-  export let activeItems;
+  export let activeItem: string;
 </script>
 
 <div class="max-w-md w-full h-screen flex-shrink flex-grow-0 px-4 ">
   <div class="sticky top-0 p-4 bg-gray-100 rounded-xl w-full h-full">
     <ul class="flex sm:flex-col overflow-hidden content-center justify-center">
       {#each items as item}
-        <li
-          on:click={() => dispatch("tabChange", item)}
-          class="py-2 hover:bg-indigo-300 rounded"
-        >
+        <li on:click={() => dispatch("tabChange", item)} class="py-2 hover:bg-indigo-300 rounded {item === activeItem ? 'bg-indigo-300' : ''}">
           <a class="truncate" href="#">
-            <img
-              src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg"
-              class="w-7 sm:mx-2 mx-4 inline"
-            />
+            <img alt="home icon" src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
             <span class="hidden sm:inline">{item}</span>
           </a>
         </li>
