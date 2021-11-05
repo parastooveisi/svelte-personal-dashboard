6442e6b5f411c47cb9916e12de68df1cd3f32d9a
Joe Krump <joe.krump@clio.com>
2021-11-04 22:50:52 -0400

    Update how tabs work

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
-  export let items;
+  export let items: { title: string; icon: string }[];
   export let activeItem: string;
 </script>
 
@@ -9,10 +9,10 @@
   <div class="sticky top-0 p-4 bg-gray-100 h-full">
     <ul class="flex sm:flex-col overflow-hidden content-center justify-center">
       {#each items as item}
-        <li on:click={() => dispatch("tabChange", item)} class="py-2 hover:bg-indigo-300 rounded {item === activeItem ? 'bg-indigo-300' : ''}">
+        <li on:click={() => dispatch("tabChange", item.title)} class="py-2 hover:text-indigo-300 rounded {item.title === activeItem ? 'text-indigo-600' : ''}">
           <a class="truncate" href="#">
-            <img alt="home icon" src="//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg" class="w-7 sm:mx-2 mx-4 inline" />
-            <span class="hidden sm:inline">{item}</span>
+            <span class="w-7 sm:mx-2 mx-4 inline">{item.icon}</span>
+            <span class="hidden sm:inline">{item.title}</span>
           </a>
         </li>
       {/each}
